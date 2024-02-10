42dca427e579f73e3deee809bce625588cfc1fe0
Jaffar Jatmiko Jati <jaffar@ainosi.co.id>
2024-02-10 10:32:09 +0700

    add: trying about

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/header/header';
+
+export default function About() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>About Us</h1>
+                <p>Welcome to our about page. We are a company dedicated to...</p>
+            </main>
+        </div>
+    );
+};
