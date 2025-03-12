78dc559111695d47333535b8988baf6df359bf34
jaffarjjati <jjaffar67@gmail.com>
2025-03-12 22:40:42 +0700

    chore: update framework and add dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        'lauren-thompson': ['Lauren Thompson', 'sans-serif'],
+        "lauren-thompson": ["Lauren Thompson", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
 export default config;
